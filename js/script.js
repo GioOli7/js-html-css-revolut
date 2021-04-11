849ee61607efdfddf10609d9594ec1a440d54a5d
GioOli7 <gio07live@gmail.com>
2021-04-11 23:28:11 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -4,21 +4,12 @@ $(document).ready(function() {
     var dropdownLink = dropdown.children('a');
     var dropdownMenu = dropdown.children('.dropdown_menu')
 
-    // problema: la funzione toggle() inserisce un display:block direttamente inline
-    // sovrascrivendomi il display:flex del menu feature. per ovviare devo, al caricamento
-    // della pagina, chiudere preventivamente tutti i menu, così posso evitare
-    // di utilizzare il display:none di default. non credo sia una solizione ottimale.
-    // dropdownMenu.hide();
-
     dropdownLink.click(function(e) {
 
         var menu = $(this).next('.dropdown_menu');
         dropdownMenu.not(menu).hide();
         menu.toggle();
     })
-
-
-
     // end document ready
 }
 )
